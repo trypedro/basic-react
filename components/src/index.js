f52feabc12e85bfa1f249449b69bac6cc85a8397
Pedro <pedroivodefaria@gmail.com>
2020-06-02 05:09:04 -0300

    Finalização do Components, e inicio sobre State e LifeCycles.

--- components/src/index.js
+++ components/src/index.js
@@ -1,29 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import faker from 'faker'
+import ApprovalCard from './ApprovalCard'
+import CommentDetail from './CommentDetail'
 
 const App = () => {
     return (
+        <div className='ui container comments' style={{padding: '1rem'}}>
 
-        <div className='ui container comments'>
-            <div className='comment'>
-                <a href='/' className='avatar'>
-                    <img alt='avatar' />    
-                </a>
-                <div className='content'>
-                    <a href='/' className='author'>
-                        Sam.
-                    </a>
-                    <div className='metadata'>
-                        <span className='date'>
-                            Today at 6:00PM
-                        </span>
-                    </div>
-                    <div className='tect'>Nice blog post!</div>
-                </div>
+            <ApprovalCard>
+                <h4>Warning!</h4>
+                Are you sure you want to do this?
+            </ApprovalCard>
 
-            </div>
+            <ApprovalCard> 
+                <CommentDetail
+                    author='Sam'
+                    timeAgo='Today at 4:45PM'
+                    avatar={faker.image.avatar()}
+                    content='Nice blog, liked!'
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author='Alex'
+                    timeAgo='Today at 2:00AM' 
+                    avatar={faker.image.avatar()} 
+                    content='I dont know what to think.' 
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author='Jane' 
+                    timeAgo='Yesterday at 2:30AM' 
+                    avatar={faker.image.avatar()} 
+                    content='Ok, but I have some questions about it.' 
+                />
+            </ApprovalCard>
         </div>
-
     );
 };
 
