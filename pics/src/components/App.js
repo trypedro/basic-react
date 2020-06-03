dbd7d0a57f9a7fd299b04c616cd2027d655d08a9
Pedro <pedroivodefaria@gmail.com>
2020-06-03 06:40:00 -0300

    Fim do seasons, e início do projeto de pics, introdução a ligação com API

---
+++ pics/src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import axios from 'axios'
+import SearchBar from './SearchBar'
+
+class App extends React.Component {
+
+    onSearchSubmit(term) {
+        console.log(term)
+    }
+    
+    render(){
+        return (
+            <div className='ui container' style={{marginTop: '20px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+        )
+    }
+}
+
+export default App;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,16 +1,39 @@
  import React from 'react'
  import ReactDOM from 'react-dom'
+ import SeasonDisplay from './SeasonDisplay'
+ import Spinner from './Spinner'
 
  class App extends React.Component {
 
-    render() {
-        
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    )
+    state = { lat: null, errorMessage: '' }
+    
+    componentDidMount(){
+        //we called setState, that came with React.Component.
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
+        );
+    }
 
-        return <div>Latitude: </div>
+    renderContent() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+
+        return <Spinner message='Please accept location request.'/>
+    }
+
+    //React says we have to define render!! 
+    render() {
+        return (
+            <div className='border red'>
+                {this.renderContent()}
+            </div>
+        )
     }
  }
 
