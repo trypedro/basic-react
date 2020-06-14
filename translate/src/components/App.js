12001664254ef7783ec8ae2a8b231f90fdca0454
Pedro Ivo <pedroivodefaria@gmail.com>
2020-06-14 19:24:52 -0300

    Learning about Context

---
+++ translate/src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import UserCreate from './UserCreate'
+import { LanguageStore } from '../context/LanguageContext'
+import ColorContext from '../context/ColorContext'
+import LanguageSelector from './LanguageSelector'
+
+class App extends React.Component{
+
+    render() {
+
+        return(
+            <div className='ui container'>
+                <LanguageStore>
+                    <LanguageSelector />
+                    <ColorContext.Provider value='green'>
+                        <UserCreate  />
+                    </ColorContext.Provider>
+                </LanguageStore>
+            </div>
+        )
+    }
+}
+
+export default App;
